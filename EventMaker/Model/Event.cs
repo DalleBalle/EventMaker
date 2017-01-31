84f0e7d0bd63eee0d62c6cc9cfc287021eb508ab
Daniel Jensen <dadanner@gmail.com>
2017-01-31 10:19:32 +0100

    Added Event class with constructor

---
+++ EventMaker/Model/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker.Model
+{
+    class Event
+    {
+        int Id;
+        string Name;
+        string Description;
+        string Place;
+        DateTime DateTime;
+
+        Event()
+        {
+            Id = 0;
+            Name = "navn";
+            Description = "beskrivelse";
+            Place = "sted";
+            DateTime = new DateTime(2017, 3, 12);
+        }
+
+
+    }
+}

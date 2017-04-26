4a3c628eda5672689082e501f102ad1388282112
Daniel <dadanner@gmail.com>
2017-04-26 17:31:00 +0200

    Lavet reference + reference constructor til eventViewModel.

--- EventMaker/Handler/EventHandler.cs
+++ EventMaker/Handler/EventHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventMaker.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,15 @@ namespace EventMaker.Handler
 {
     class EventHandler
     {
-        
+        // reference til eventViewModel'en
+        public EventViewModel EventViewModel { get; set; }
+
+        // eventViewModel reference constructor
+        public EventHandler (EventViewModel eventViewModel)
+        {
+            EventViewModel = eventViewModel;
+        }
+
+
     }
 }
